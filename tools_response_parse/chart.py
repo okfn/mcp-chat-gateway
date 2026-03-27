498a6a62d2d090a7b6d3a0056bef2445ebab988f
avdata99 <andres@data99.com.ar>
2026-03-27 14:19:16 -0300

    Allow charts

---
+++ tools_response_parse/chart.py
@@ -0,0 +1,32 @@
+import json
+import re
+
+
+def parse_chart_response(data, response):
+    """
+    Extract from raw response all code in between <chart> and </chart>
+    and put it in data['chart'].
+
+    Expected JSON format:
+    {
+        "type": "bar",
+        "title": "Monthly Revenue",
+        "labels": ["Jan", "Feb", "Mar"],
+        "values": [100, 200, 150],
+        "beginAtZero": false (default true)
+    }
+    """
+
+    pattern = r'<chart>(.*?)</chart>'
+    matches = re.findall(pattern, response, re.DOTALL)
+    if matches:
+        data['chart'] = matches[0].strip()
+        # Remove the <chart>...</chart> part from the response
+        response = re.sub(pattern, '', response, flags=re.DOTALL).strip()
+
+        try:
+            data['chart_data'] = json.loads(data['chart'])
+        except json.JSONDecodeError:
+            data['chart_error'] = 'Error parsing chart JSON'
+
+    return data, response
